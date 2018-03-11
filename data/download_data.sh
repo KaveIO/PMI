9414ca799dc89f16b84edc3282ff0fb07e45c12e
Max Baak <Baak.Max@kpmg.nl>
2018-03-11 11:47:24 +0100

    Data: update of download_data.sh script to reflect final datasets

--- data/download_data.sh
+++ data/download_data.sh
@@ -14,22 +14,5 @@ echo ""
 echo "Now downloading the processed DBLP datasets and external datasets."
 echo ""
 
-# --- all derived datasets
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/Data.7z
-7zr x Data.7z 
-
-# --- page rank and citations
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/dblp.v10.zip
-unzip dblp.v10.zip
-
-# --- word2vec models
-mkdir word2vec_models
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/word2vec_models/model_1520420116 -P word2vec_models/
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/word2vec_models/model_1520420191 -P word2vec_models/
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/word2vec_models/model_abstract_1520532394 -P word2vec_models/
-
-# --- custom mentor-mentee preferences files for demonstration
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/custom_mentees_preferencies.xlsx
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/custom_mentees_topic_preferencies.xlsx
-wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/mentee_mentors_match.csv
-
+wget -c https://s3-eu-west-1.amazonaws.com/kct.bigdata/projects/exercise-solutions/dblp/pmi_data.zip
+unzip pmi_data.zip
